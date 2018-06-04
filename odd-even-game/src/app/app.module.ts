681197053fdde4d5818e850b51d368de7766b20e
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-06-05 01:12:40 +0530

    Finalizing project

--- odd-even-game/src/app/app.module.ts
+++ odd-even-game/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent
+    GameControlComponent,
+    OddComponent,
+    EvenComponent
   ],
   imports: [
     BrowserModule
--- odd-even-game/src/app/game-control/game-control.component.ts
+++ odd-even-game/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastnumber = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartGame() {
+this.interval = setInterval(() => {
+  this.intervalFired.emit(this.lastnumber + 1);
+this.lastnumber++;
+}, 1000 );
+  }
+
+  onPauseGame() {
+    clearInterval(this.interval);
   }
 
 }
